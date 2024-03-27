1d086b844c164d1d1b16edf031cdb2ace76ba65c
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-03-26 17:47:36 -0700

    Create ui_manager.js

---
+++ scripts/ui_manager.js
@@ -0,0 +1,19 @@
+class UIManager {
+    constructor() {
+        this.uiElements = [];
+    }
+
+    addElement(element) {
+        this.uiElements.push(element);
+    }
+
+    render(ctx) {}
+
+    handleInput(mouseX, mouseY, click) {
+        for (const element of this.uiElements) {
+            if (element.handleClick && click) {
+                element.handleClick(mouseX, mouseY);
+            }
+        }
+    }
+}
