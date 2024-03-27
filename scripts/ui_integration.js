739b9a05a40ee3d6aa78d692624370e75c25a780
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-03-26 17:48:11 -0700

    Create ui_integration.js

---
+++ scripts/ui_integration.js
@@ -0,0 +1,14 @@
+const uiManager = new UIManager();
+
+const playButton = new Button(100, 100, 100, 50, "Play", () => {});
+
+uiManager.addElement(playButton);
+
+function gameLoop() {}
+
+canvas.addEventListener('click', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+    uiManager.handleInput(mouseX, mouseY, true);
+});
