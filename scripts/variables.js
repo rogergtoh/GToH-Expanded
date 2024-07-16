d26ce362772d9776e0858e4c7a52d00647749dfb
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-07-15 22:11:18 -0700

    0.4.5

--- scripts/variables.js
+++ scripts/variables.js
@@ -2,7 +2,7 @@ console.log("loading variables...")
 var warning = false; //set to true if redesgining code that could potentially break game
 
 // Game version!!
-const GAMEVERSION = [0,4,4];
+const GAMEVERSION = [0,4,5];
 
 // These are for checking server compatibility, only change these if updating information sent/received by the server
 const ClientVersion = 1;
