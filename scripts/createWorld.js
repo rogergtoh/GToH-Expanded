da76afe76029f2c2e327cc923aa21c59d681f0ee
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-07-17 17:37:24 -0700

    Orbo's Skins Part 2 (The Back World)

--- scripts/createWorld.js
+++ scripts/createWorld.js
@@ -132,7 +132,7 @@ function CreateWorld(id, useID = true) {
     //worldText.push(new Text('L to set username', 670, -100, 12));
     worldText.push(new Text('Set username for players to see you (L)', 670, -20, 12));
     worldText.push(new Text('GToH Expanded ' + GAMEVERSION.join("."), 670, -40, 12));
-    worldText.push(new Text('GToH Expanded ' + LevelRewards.WorldOneToken, 600, 200, 12));
+    worldText.push(new Text('World One Tokens: ' + LevelRewards.WorldOneToken, 600, 200, 12));
     world.push(new AnimatedBlock(95, 260, 'orbo.png', t, 2, ['nc']));
     //worldText.push(new Text('Coming March 19...', 360, -320, 12));
     world.push(new AnimatedBlock(635, -320, 'portalgreyanim.png',
