726d6caa595e2d912f7ef77f192571969228a321
guppyadrian <guppyadrian@gmail.com>
2024-08-20 19:58:18 -0800

    added tryBuy() func

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,3 +1,15 @@
 const InventoryItems = {
-  "item1": {StyleToken: 2}
+  "item1": {StyleToken: 2, Orbuck: 2}
 };
+
+function tryBuy(itemName, allowMultiple = false) {
+  if (!checkReqs(InventoryItems[itemName])) return false;
+  
+  if (allowMultiple && (itemName in Inventory)) {
+    Inventory[itemName] += 1;
+  } else {
+    Inventory[itemName] = 1;
+  }
+  syncRewards();
+  return true;
+}
