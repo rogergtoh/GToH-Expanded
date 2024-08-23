b6e1823a4d2f7e3dd8470c64cbb4fce88ebdcf8a
rogergtoh <157563838+rogergtoh@users.noreply.github.com>
2024-08-22 22:17:44 -0700

    inventory fix 4

--- scripts/shop.js
+++ scripts/shop.js
@@ -17,7 +17,7 @@ function tryBuy(itemName, allowMultiple = false) {
   } else {
     Inventory[itemName] = 1;
   }
-  localStorage.setItem('inventory');
+  localStorage.setItem('inventory', JSON.stringify(inventory));
   syncRewards();
   return true;
 }
